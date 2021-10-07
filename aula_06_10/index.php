8d3e8918e5c3bfa41a941c93419703d36f84271a
Welliton Luiz Becker <welliton.luiz@unidavi.edu.br>
2021-10-06 21:34:42 -0300

    criado

--- aula_06_10/index.php
+++ aula_06_10/index.php
@@ -1,4 +1,21 @@
 <?php
 
     include 'conexao.php';
+    $oSql = $oConn->prepare(
+        'SELECT CIDADES.*,
+                ESTADOS.SIGLA AS ESTADO_SIGLA,
+                ESTADOS.NOME  AS ESTADO_NOME
+           FROM CIDADES, ESTADOS
+          WHERE CIDADES.ESTADO = ESTADOS.ID');
+
+    $oSql->execute();
+    $aResult = $oSql->fetchAll();
+    print_r($aResult);
+
+    foreach ($aResult as $value) {
+        foreach ($value as $key => $value) {
+            echo $key . ' => ' . $value . '</br>';
+        }
+    }
+    
 ?>
